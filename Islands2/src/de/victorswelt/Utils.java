529ed3260899abccbb5d4cbc204f6336386483d9
TheZeldakatze <victor@wenzeslaus.de>
2020-12-14 08:24:40 +0100

    Added VarInt a VarInt decoder and encoder

--- Islands2/src/de/victorswelt/Utils.java
+++ Islands2/src/de/victorswelt/Utils.java
@@ -1,10 +1,16 @@
 package de.victorswelt;
 
 import java.awt.Font;
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
 
 public class Utils {
 	public static Font FONT_HEADER  = new Font(Font.SANS_SERIF, Font.BOLD, 24),
-					   FONT_DEFAULT = new Font(Font.SANS_SERIF, Font.PLAIN, 14);
+					   FONT_DEFAULT = new Font(Font.SANS_SERIF, Font.PLAIN, 14),
+					   FONT_MONO    = new Font(Font.MONOSPACED, Font.PLAIN, 14);
+	
 	
 	private Utils() {}
 	
@@ -16,5 +22,43 @@ public class Utils {
 		if(x2 > x1 + w1) return false;
 
 		return true;
+	}
+	
+	public static int decodeVarNum(DataInputStream dis) throws IOException, NumberFormatException {
+		int ret = 0;
+		int bytePosition = 0;
+		
+		while(true) {
+			int in = dis.readByte();
+			ret |= (in & 0x7F) << bytePosition * 7;
+			bytePosition++;
+			
+			if(bytePosition>9)
+				throw new NumberFormatException("VarNum too big");
+			
+			// the input does not have any more bytes following
+			if((in & 0x80) == 0)
+				break;
+		}
+		
+		return ret;
+	}
+	
+	public static void encodeVarInt(DataOutputStream dos, int number) throws IOException, NumberFormatException {
+		if(number<0)
+			throw new NumberFormatException("VarNum is negative");
+		
+		while(true) {
+            if ((number & 0xFFFFFF80) == 0) {
+                dos.writeByte((int) number);
+                return;
+            }
+			
+			// 
+			dos.writeByte(((int) (number & 0x7F)) | 0x80);
+			
+			// shift the number to the right
+			number >>>= 7;
+		}
 	}
 }
